52e3923a0db8b9369c863a8a1ba17ca3b2772584
yamadamasashi <shisama94@gmail.com>
2023-06-22 09:44:16 +0900

    testを書く

--- tax-api/src/calcTax.test.ts
+++ tax-api/src/calcTax.test.ts
@@ -2,21 +2,70 @@ import { calcRetirementIncomeDeduction } from './calcTax'
 
 test('テスト名', () => {
   const deduction = calcRetirementIncomeDeduction({
-    yearsObService: 2,
+    yearsOfService: 2,
     isDisability: false,
   })
   expect(deduction).toBe(800_000)
 })
 
 describe('テスト名', () => {
+  describe('勤続年数1年の場合', () => {
+    describe('障害者となったことに直接起因して退職に該当しない', () => {
+      test.each`
+        yearsOfService | expected
+        ${1}           | ${800_000}
+      `(
+        '眷属年数$yearsOfService -> $expected円',
+        ({ yearsOfService, expected }) => {
+          const deduction = calcRetirementIncomeDeduction({
+            yearsOfService,
+            isDisability: false,
+          })
+          expect(deduction).toBe(expected)
+        },
+      )
+    })
+    describe('障害者となったことに直接起因して退職に該当する', () => {
+      test.each`
+        yearsOfService | expected
+        ${1}           | ${1_800_000}
+      `(
+        '眷属年数$yearsOfService -> $expected円',
+        ({ yearsOfService, expected }) => {
+          const deduction = calcRetirementIncomeDeduction({
+            yearsOfService,
+            isDisability: true,
+          })
+          expect(deduction).toBe(expected)
+        },
+      )
+    })
+  })
+
   test.each`
     yearsOfService | isDisability | expected
     ${1}           | ${false}     | ${800_000}
     ${2}           | ${false}     | ${800_000}
     ${3}           | ${false}     | ${1_200_000}
-    ${4}           | ${false}     | ${7600_000}
-    ${3}           | ${false}     | ${8_000_000}
-    ${3}           | ${false}     | ${8_700_000}
-    ${3}           | ${false}     | ${15_200_000}
-  `
+    ${19}          | ${false}     | ${7_600_000}
+    ${20}          | ${false}     | ${8_000_000}
+    ${21}          | ${false}     | ${8_700_000}
+    ${30}          | ${false}     | ${15_000_000}
+    ${1}           | ${true}      | ${1_800_000}
+    ${2}           | ${true}      | ${1_800_000}
+    ${3}           | ${true}      | ${2_200_000}
+    ${19}          | ${true}      | ${8_600_000}
+    ${20}          | ${true}      | ${9_000_000}
+    ${21}          | ${true}      | ${9_700_000}
+    ${30}          | ${true}      | ${16_000_000}
+  `(
+    '$yearsOfService年 isDisability:$isDisability→ $expected',
+    ({ yearsOfService, isDisability, expected }) => {
+      const deduction = calcRetirementIncomeDeduction({
+        yearsOfService,
+        isDisability,
+      })
+      expect(deduction).toBe(expected)
+    },
+  )
 })
--- tax-api/src/calcTax.ts
+++ tax-api/src/calcTax.ts
@@ -1,10 +1,22 @@
-type CalcRetirementIncomeDeduction = {
-  yearsObService: number
+type CalcRetirementIncomeDeductionInput = {
+  yearsOfService: number
   isDisability: boolean
 }
 
-export const calcRetirementIncomeDeduction = (
-  input: CalcRetirementIncomeDeduction,
-) => {
-  return 800_000
+export const calcRetirementIncomeDeduction = ({
+  yearsOfService,
+  isDisability,
+}: CalcRetirementIncomeDeductionInput) => {
+  let result
+  if (yearsOfService === 1) {
+    result = 800_000
+  } else if (yearsOfService <= 19) {
+    result = 400_000 * yearsOfService
+  } else {
+    result = 8_000_000 + 700_000 * (yearsOfService - 20)
+  }
+  if (isDisability) {
+    result += 1_000_000
+  }
+  return result
 }
